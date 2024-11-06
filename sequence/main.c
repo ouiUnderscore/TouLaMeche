3f8a81cfb4dcaca4548df48cdf1506db08491f63
oui_ <emilie.zammit29@gmail.com>
2024-11-06 17:43:02 +0100

    position+iterator fonctionnel

--- sequence/main.c
+++ sequence/main.c
@@ -6,40 +6,56 @@
 
 int main()
 {
-    // Crée un tableau pour tester le N-gramme
     struct strhash_table *ht = strhash_create(Lg_N_gramme);
-    
-    // Initialise le N-gramme
     sequence_initialize(ht);
 
     // Test 1 : Ajout de mots dans le N-gramme
-    sequence_addWord("Hello", ht);
-    sequence_progress();  // Avance le buffer circulaire
-    sequence_addWord("world", ht);
-    sequence_progress();  // Avance le buffer circulaire
-    sequence_addWord("this", ht);
-    sequence_progress();  // Avance le buffer circulaire
+    printf("Ajout des mots dans le N-gramme :\n");
+    sequence_addWord("aaa", ht);
+    sequence_progress();
+    sequence_addWord("bbb", ht);
+    sequence_progress();
+    sequence_addWord("ccc", ht);
+    sequence_progress();
 
-    // Test 2 : Affichage du N-gramme courant
-    printf("N-gramme après ajout de 3 mots :\n");
-    sequence_print();  // Affiche "Hello/world/this/"
+    // Test 2 : Affichage du N-gramme après l'ajout de 3 mots
+    printf("\nN-gramme après ajout de 3 mots (attendu \"aaa/bbb/ccc/\") :\n");
+    sequence_print();
 
     // Test 3 : Ajout d'un autre mot, et "avancement" du buffer circulaire
-    sequence_addWord("is", ht);
-    sequence_progress();  // Avance le buffer circulaire
+    printf("\nAjout du mot 'ddd' et avancement du buffer :\n");
+    sequence_addWord("ddd", ht);
+    sequence_progress();
 
-    // Test 4 : Affichage du N-gramme après avoir ajouté un mot supplémentaire
-    printf("\nN-gramme après ajout du mot 'is' :\n");
-    sequence_print();  // Affiche "world/this/is/"
+    // Test 4 : Affichage du N-gramme après avoir ajouté le mot 'ddd'
+    printf("\nN-gramme après ajout du mot 'ddd' (attendu \"ddd/bbb/ccc/\") :\n");
+    sequence_print();
 
     // Test 5 : Test de l'itérateur
-    sequence_itStart();  // Démarre l'itérateur
+    sequence_itStart();
 
-    printf("\nUtilisation de l'itérateur :\n");
-    while (sequence_itHasNext()) {
+    printf("\nUtilisation de l'itérateur (attendu \"bbb ccc ddd \") :\n");
+    while (sequence_itHasNext())
         printf("%s ", sequence_itNext());
+    printf("\n");
+
+    // Test 6 : Ajouter un autre mot pour voir l'évolution de l'itérateur
+    printf("\nAjout d'un mot supplémentaire : 'eee'\n");
+    sequence_addWord("eee", ht);
+    sequence_progress();
+
+    // Test 7 : Affichage du N-gramme après l'ajout du mot "eee"
+    printf("\nN-gramme après ajout du mot 'eee' (attendu \"ddd/eee/ccc/\") :\n");
+    sequence_print();
+
+    // Test 8 : Tester l'itérateur après ajout d'un nouveau mot
+    sequence_itStart();
+
+    printf("\nUtilisation de l'itérateur après ajout du mot 'eee' (attendu \"ccc ddd eee \") :\n");
+    while (sequence_itHasNext())
+    {
+        printf("%s ", sequence_itNext()); // Affiche chaque mot et son indice dans le parcours
     }
-    // Cela devrait afficher "world this is "
     printf("\n");
 
     return 0;
--- sequence/sequence.c
+++ sequence/sequence.c
@@ -4,17 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static int iterator;
-static char *n_gramme[Lg_N_gramme];
+static int iterator; // Iterateur pour parcourir les éléments du n_gramme
+static int position; // Position de la prochaine écriture
+static char *n_gramme[Lg_N_gramme + 1];
 static char *next_word;
 
 // initialise le N-gramme avec des mots vides
 void sequence_initialize(struct strhash_table *ht)
 {
-    ht = strhash_create(Lg_N_gramme);
-
-    for (int i = 0; i < ht->size; i++)
+    for (int i = 0; i < (Lg_N_gramme + 1); i++)
         n_gramme[i] = NULL; // strhash_wordAdd(ht, NULL);
+
+    position = 1;
 }
 
 // Gestion de l'iterateur permettant de parcourir le N-gramme
@@ -22,21 +23,21 @@ void sequence_initialize(struct strhash_table *ht)
 void sequence_itStart(void)
 {
     // L'itérateur c'est après le mot courant, c'est la position où on peut écrire
-    iterator = 0;
+    iterator = (position + 1) % (Lg_N_gramme + 1);
 }
 
 // retourne le mot correspondant a la position de l'iterateur avance la position de l'iterateur.
 const char *sequence_itNext(void)
 {
     const char *current_word = n_gramme[iterator];
-    iterator = (iterator + 1) % Lg_N_gramme;  // Avance circulairement
+    iterator = (iterator + 1) % (Lg_N_gramme + 1); // Avance circulairement
     return current_word;
 }
 
 // test si l'iterateur est a la fin du N-gramme
 int sequence_itHasNext(void)
 {
-    // TODO:
+    return iterator != position;
 }
 
 // Nouveau mot de fin du prochain N-gramme definit le nouveau mot qui entrera dans le N-gramme
@@ -55,11 +56,17 @@ const char *sequence_nextWord(void)
 void sequence_progress(void)
 {
     // Ajoute le nouveau mot à la position courante de l'itérateur
-    n_gramme[iterator] = next_word;
+    n_gramme[position] = next_word;
 
     // Met à jour l'index de l'itérateur pour "avancer" le buffer circulaire.
     // Si iterator atteint Lg_N_gramme, il revient à 0
-    iterator = (iterator + 1) % Lg_N_gramme;
+    position = (position + 1) % (Lg_N_gramme + 1);
+
+    // Réinitialise la case précédente avant de mettre à jour la position
+    if (n_gramme[position] != NULL)
+    {
+        n_gramme[position] = NULL; // Réinitialise la case actuelle
+    }
 }
 
 //
@@ -70,9 +77,9 @@ void sequence_print(void)
 {
     int it_temp = iterator;
 
-    for (int i = 0; i < Lg_N_gramme; i++)
+    for (int i = 0; i < (Lg_N_gramme + 1); i++)
     {
-        if (it_temp == Lg_N_gramme)
+        if (it_temp == (Lg_N_gramme + 1))
         {
             printf("%s/", n_gramme[i]);
             it_temp = 0;
@@ -81,6 +88,26 @@ void sequence_print(void)
         {
             printf("%s/", n_gramme[i]);
             it_temp++;
+        }
+    }
+    printf("\n");
+    
+    // Sans les nulls
+    it_temp = iterator;
+    for (int i = 0; i < (Lg_N_gramme + 1); i++)
+    {
+        if (n_gramme[i] != NULL)
+        {
+            if (it_temp == (Lg_N_gramme + 1))
+            {
+                printf("%s/", n_gramme[i]);
+                it_temp = 0;
+            }
+            else
+            {
+                printf("%s/", n_gramme[i]);
+                it_temp++;
+            }
         }
     }
     printf("\n");
